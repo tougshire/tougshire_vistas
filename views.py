18dbe9bd761c7f18aaa36b37c69b93348460fd36
tougshire <tougshire@tougshire.com>
2022-08-11 21:04:13 -0400

    Remove troubleshooting print commands

--- views.py
+++ views.py
@@ -218,14 +218,12 @@ def make_vista_fields(model, field_names=[]):
         field_names = [ field.name for field in model._meta.get_fields() ]
 
     for field_name in field_names:
-        print('tp 2259h55',)
         chained_label = ''
         if '__' in field_name:
             chain_model = model
             chain = field_name.split('__')
             for l in range( len( chain ) -1 ):
                 try:
-                    print('tp 2259i03', chain[l])
                     chained_label = chained_label + chain_model._meta.get_field(chain[l]).verbose_name.title() + ' '
                 except AttributeError as e:
                     chained_label = chained_label + chain_model._meta.get_field(chain[l]).name.title() + ' '
